c62e30b6c1de9c2f3298a3e7287a6691cd96e118
Angel Valdes <AngelValdes@hotmail.com>
2016-08-22 20:30:51 -0400

    v1.0.10

--- logger/test/__index.js
+++ logger/test/__index.js
@@ -1,10 +1,12 @@
 ﻿//const assert = require('assert');
 //const expect = require('chai').expect;
+const logger = require('../index.js');
 
 global.DEBUG = true;
 
 describe('test logger', () => { //to be implemented
     it('test debug function', () => {
+        logger.debug("test message", 0);
         //assert.equal();
         //expect('testValue').toBe('testValue');
     });
