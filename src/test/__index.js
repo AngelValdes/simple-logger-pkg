2628e6741b3df36341e8e802aa4cc93153839ecd
Angel Valdes <Angel Valdes>
2016-08-29 10:55:54 -0400

    test update

--- src/test/__index.js
+++ src/test/__index.js
@@ -2,18 +2,6 @@
 const utility = require('../index');
 var expect = require('chai').expect;
 
-describe('test utility.versionUp()', () => {
-    const currentVersion = "1.0.1";
-    it('return incremented version Major', () => {
-        assert.equal(utility.versionUp(currentVersion, "Major"), "2.0.1");
-    });
-    it('return incremented version Minor', () => {
-        assert.equal(utility.versionUp(currentVersion, "Minor"), "1.1.1");
-    });
-    it('return incremented version Patch', () => {
-        assert.equal(utility.versionUp(currentVersion, "Patch"), "1.0.2");
-    });
-});
 describe('test utility.debug()', () => {
     global.DEBUG = true;
     const message = "some message";
